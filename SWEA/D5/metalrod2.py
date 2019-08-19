417067b0e0090cae9b08869eec881b8219dcbabe
SeolYu Lee <seolyu.90@gmail.com>
2019-08-19 10:08:18 +0900

    metalrod2 | JP advice

--- SWEA/D5/metalrod2.py
+++ SWEA/D5/metalrod2.py
@@ -2,14 +2,14 @@
 
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     n = int(input())
     metalnum = list(map(int, input().split()))
 
     nuts = metalnum[1::2]  # 암나사 크기
 
     # metals에 각각의 원형 금속 막대를 리스트로 append
-    metals = []  
+    metals = []
     for i in range(n):
         metals.append([metalnum.pop(0), metalnum.pop(0)])
 
@@ -26,9 +26,9 @@ for tc in range(1, T+1):
             if metals[k][0] == result[-1][1]:
                 result.append(metals[k])
                 break
-    
-    print ('#', tc, sep='', end=' ')
-    
+
+    print('#', tc, sep='', end=' ')
+
     for l in range(len(result)):
         for m in range(len(result[l])):
             print(result[l][m], end=' ')
