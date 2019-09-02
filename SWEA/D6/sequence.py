983b446cad3d11be1ecc5ccc5916c71bf7dad92d
SeolYu, Lee <seolyu.90@gmail.com>
2019-09-02 18:19:23 +0900

    20190902

---
+++ SWEA/D6/sequence.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+
+def display(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print(arr[i][j], end=' ')
+        print()
+    print()
+
+
+for tc in range(3):
+    V, E = map(int, input().split())
+    temp = list(map(int, input().split()))
+    board = []
+
+    for i in range(V + 1):
+        t = []
+        for j in range(V + 1):
+            t.append(0)
+        board.append(t)
+
+    for i in range(E):
+        board[temp[2 * i]][temp[2 * i + 1]] = 1
+
