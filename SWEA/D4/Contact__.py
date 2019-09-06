d569844de22540eaf184149baf863638f9ea135e
SeolYu, Lee <seolyu.90@gmail.com>
2019-09-06 10:32:15 +0900

    20190906

--- SWEA/D4/Contact__.py
+++ SWEA/D4/Contact__.py
@@ -15,14 +15,31 @@ for tc in range(10):
     visited[0] = True
     queue = [s]
     path = []
+    queue_max = []
 
     while queue:
         t = queue.pop(0)
+        next_list = contact[t]
+        contact[t] = []
+
         if visited[t] == False:
             visited[t] = True
             path.append(t)
-        for i in contact[t]:
+
+            queue_max.append(next_list)
+
+        for i in next_list:
             if visited[i] == False:
                 queue.append(i)
+        # if visited[t] == False:
+        #     visited[t] = True
+        #     path.append(t)
+        #
+        #     queue_max.append(contact[t])
+        # for i in contact[t]:
+        #     if visited[i] == False:
+        #         queue.append(i)
+
 
     print(path)
+    print(queue_max)
