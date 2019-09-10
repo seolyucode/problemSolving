cf396c31fd63ce015b0933cef4e10c355062f3a6
SeolYu, Lee <seolyu.90@gmail.com>
2019-09-10 13:15:35 +0900

    20190910

---
+++ SWEA/D2/subtree.py
@@ -0,0 +1,15 @@
+# 5174. [파이썬 S/W 문제해결 기본] 8일차 - subtree
+
+T = int(input())
+
+for tc in range(1, T+1):
+    E, N = map(int, input().split())
+
+    templ = list(map(int, input().split()))
+
+    tree = [[0]*2 for _ in range(E+2)]
+
+    for i in range(E):
+        templ[2*i]
+
+    print(templ)
