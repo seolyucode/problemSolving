d11096f082d00692d02041a29ee85f97ee0c4016
SeolYu, Lee <seolyu.90@gmail.com>
2020-02-23 11:11:00 +0900

    lambda

---
+++ Python/11.py
@@ -0,0 +1,21 @@
+'''
+람다 함수(익명의 함수. 람다 표현식)
+
+def plus_one(x):
+    return x+1
+
+print(plus_one(1))
+
+plus_two=lambda x: x+2
+print(plus_two(1))
+'''
+
+
+def plus_one(x):
+    return x+1
+
+# 내장함수 인자로 쓸 때 유용
+a=[1, 2, 3]
+print(list(map(plus_one, a)))
+
+print(list(map(lambda x: x+1, a)))
