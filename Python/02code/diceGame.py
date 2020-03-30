e81aaae673b6dcbd75a19dab95273ae99790dbb3
SeolYu <seolyu.90@gmail.com>
2020-03-30 21:40:57 +0900

    diceGame

---
+++ Python/02code/diceGame.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("input9.txt", "r")
+
+n = int(input())
+res = 0
+for i in range(n):
+    tmp = input().split()
+    tmp.sort()
+    a, b, c = map(int, tmp)
+    if a==b and b==c:
+        money = 10000 + a*1000
+    elif a==b or a==c:
+        money = 1000 + (a*100)
+    elif b==c:
+        moeny = 1000 + (b*100)
+    else:
+        meony = c*100
+    
+    if money > res:
+        res = money
+print(res)
