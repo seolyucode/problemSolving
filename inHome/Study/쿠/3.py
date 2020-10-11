68ecc1086b012d15d2abbee7834bd3e6773325a5
SeolYu, Lee <seolyu.90@gmail.com>
2020-10-11 15:34:01 +0900

    예외처리의 중요성

--- inHome/Study/쿠/3.py
+++ inHome/Study/쿠/3.py
@@ -8,7 +8,9 @@ def solution(k, score):
             continue
         else:
             check.append(i)
+    # 예외처리해야함
     return len(check)-1
 
 print(solution(3, [24, 22, 20, 10, 5, 3, 2, 1]))
 print(solution(2, [1300000000,700000000,668239490,618239490,568239490,568239486,518239486,157658638,157658634,100000000,100]))
+print(solution(3, [24, 23, 22, 21, 20, 19, 18, 17]))
